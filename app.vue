b64fb967c715c88ad7a84321525acccd443be38a
Tak Castel <takcastel@gmail.com>
2024-04-17 17:14:45 +0200

    prepare jumbotron

--- app.vue
+++ app.vue
@@ -5,33 +5,34 @@
       <h1 class="text-white text-3xl font-semibold">Mon Portfolio</h1>
     </header>
 
-    <!-- Accueil -->
-    <section class="container mx-auto py-12">
-      <h2 class="text-2xl font-semibold mb-4">
-        À propos de moi
-      </h2>
-      <p class="text-lg">
-        Développeur spécialisé en Vue.js, axé sur le développement d'applications métier et le design UX. Mon expertise vise à créer des solutions web intuitives et efficaces, tout en contribuant à la transformation digitale des entreprises.
-      </p>
+    <section class="relative h-screen bg-cover bg-center" style="background-image: url('images/jumbotron.png'); filter: opacity(0.8);">
+      <div class="container mx-auto px-4 md:px-0 h-full flex items-center justify-center">
+        <div class="text-center text-white">
+          <h2 class="text-4xl font-semibold mb-4">
+            À propos de moi
+          </h2>
+          <p class="text-lg">
+            Développeur spécialisé en Vue.js, axé sur le développement d'applications métier et le design UX. Mon expertise vise à créer des solutions web intuitives et efficaces, tout en contribuant à la transformation digitale des entreprises.
+          </p>
+        </div>
+      </div>
     </section>
 
+
     <!-- Portfolio -->
-    <section id="portfolio" class="container mx-auto py-12">
+    <section id="portfolio" class="container mx-auto px-4 md:px-0 py-12">
       <h2 class="text-2xl font-semibold mb-4">
         Portfolio
       </h2>
 
       <!-- Cartes des projets -->
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <div class="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
         <!-- Utilisation de v-for pour créer dynamiquement les cartes -->
         <div 
           v-for="(item, index) in projects" 
           :key="index" 
-          class="relative group bg-slate-950 rounded-lg overflow-hidden shadow-md hover:scale-105 hover:shadow-xl hover:bg-fuchsia-950 hover:border-pink-500 hover:rotate-1 transition duration-300 ease-in-out"
-        >
-          <!-- Effet de bordure lumineuse -->
-          <div class="absolute inset-0 border border-purple-600 rounded-lg opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out pointer-events-none z-10"></div>
-          
+          class="flex flex-col relative group bg-slate-950 rounded-lg overflow-hidden shadow-md hover:scale-105 hover:shadow-xl hover:bg-gradient-to-r hover:from-fuchsia-900 hover:to-fuchsia-950 transition duration-300 ease-in-out"
+        >          
           <!-- Image (si présente) -->
           <img 
             v-if="item.image" 
@@ -40,20 +41,21 @@
             class="w-full h-48 object-cover z-20" 
           />
 
-          <div class="p-4 relative z-20 h-full">
+          <div class="p-4 pb-0">
             <!-- Titre -->
-            <h3 class="text-xl font-semibold mb-2 text-white group-hover:text-amber-400 transition duration-120 ease-in-out z-20">{{ item.title }}</h3>
+            <h3 class="text-xl font-semibold mb-2 text-white transition duration-120 ease-in-out z-20">{{ item.title }}</h3>
             <!-- Description -->
             <p class="text-slate-500 mb-4 z-20 group-hover:text-slate-300 transition duration-120 ease-in-out">{{ item.description }}</p>
             <!-- Lien -->
-            <a :href="item.link" class="text-blue-500 underline z-20">Voir plus</a>
           </div>
+
+          <a :href="item.link" target="_blank" class="p-4 pt-0 text-amber-400 underline mt-auto">Voir plus</a>
         </div>
       </div>
     </section>
 
   <!-- Description des services -->
-    <section class="container mx-auto py-12">
+    <section class="container mx-auto px-4 md:px-0 py-12">
       <!-- Développement Frontend -->
       <div class="flex flex-col md:flex-row md:items-center mb-8">
         <!-- Image à gauche pour desktop -->
@@ -105,7 +107,7 @@
     </section>
 
     <!-- Contact -->
-    <section class="container mx-auto py-12">
+    <section class="container mx-auto px-4 md:px-0 py-12">
       <h2 class="text-2xl font-semibold mb-4">
         Contact
       </h2>
@@ -167,24 +169,24 @@ export default {
           image: "images/portfolio-mediameeting.png",
           link: "https://www.mediameeting.fr/"
         },
-        {
-          title: "Mybeautyfullradio",
-          description: "Développement de l'application web Mybeautyfullradio en tant que développeur à Mediameeting",
-          image: "images/portfolio-wella.png",
-          link: "https://www.mediameeting.fr/portfolio/wella-mybeautyfullradio/"
-        },
-        {
-          title: "Gamestory",
-          description: "Mini-jeu dont vous êtes le héros, réalisé en React en 2017 pour apprendre le langage",
-          image: "images/portfolio-gamestory.png",
-          link: "https://takcastel.github.io/gamestory/"
-        },
-        {
-          title: "SWOR",
-          description: "Création d'un forum et scénariste pour une communauté de rôlistes sur Star Wars en 2013",
-          image: "images/portfolio-swor.png",
-          link: "http://www.swor-jdr.com/"
-        },
+        // {
+        //   title: "Mybeautyfullradio",
+        //   description: "Développement de l'application web Mybeautyfullradio en tant que développeur à Mediameeting",
+        //   image: "images/portfolio-wella.png",
+        //   link: "https://www.mediameeting.fr/portfolio/wella-mybeautyfullradio/"
+        // },
+        // {
+        //   title: "Gamestory",
+        //   description: "Mini-jeu dont vous êtes le héros, réalisé en React en 2017 pour apprendre le langage",
+        //   image: "images/portfolio-gamestory.png",
+        //   link: "https://takcastel.github.io/gamestory/"
+        // },
+        // {
+        //   title: "SWOR",
+        //   description: "Création d'un forum et scénariste pour une communauté de rôlistes sur Star Wars en 2013",
+        //   image: "images/portfolio-swor.png",
+        //   link: "http://www.swor-jdr.com/"
+        // },
       ]
     };
   }
